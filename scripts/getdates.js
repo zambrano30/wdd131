88ea0597f8e2d264ce80f3c4d0111122a87ebde9
chriszam <czambranovillavicenc@byupathway.edu>
2025-05-17 15:33:24 -0500

    Refactor index.html and styles.css for improved structure and aesthetics; add dynamic footer functionality

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Set current year in the footer
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Set last modified date in the footer
+document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
